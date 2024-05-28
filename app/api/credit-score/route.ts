1fb793e61948913715c2178b5ea5bccfa6f2b861
Mark <mark.collins@driva.com.au>
2024-05-28 14:46:02 +1000

    updates to enable prod

--- app/api/credit-score/route.ts
+++ app/api/credit-score/route.ts
@@ -1,17 +1,25 @@
-import { NextRequest,  NextResponse } from "next/server";
+import { headers } from 'next/headers'
+import { NextRequest,  NextResponse } from "next/server"
 import { z } from 'zod'
  
+const EQUIFAX_USERNAME_DEV = process.env.EQUIFAX_USERNAME_DEV ?? ''
+const EQUIFAX_USERNAME_PROD = process.env.EQUIFAX_USERNAME_PROD ?? ''
 
-const ENV = process.env.ENV
-const IS_PROD = ENV === 'production'
-const EQUIFAX_USERNAME = process.env.EQUIFAX_USERNAME
-const EQUIFAX_PASSWORD = process.env.EQUIFAX_PASSWORD
-const EQUIFAX_API_PROD = 'https://apiconnect.equifax.com.au/sys2/soap11/score-seeker-v1-0'
+const EQUIFAX_PASSWORD_DEV = process.env.EQUIFAX_PASSWORD_DEV ?? ''
+const EQUIFAX_PASSWORD_PROD = process.env.EQUIFAX_PASSWORD_PROD ?? ''
+
 const EQUIFAX_API_DEV = 'https://ctaau.apiconnect.equifax.com.au/cta/sys2/soap11/score-seeker-v1-0'
+const EQUIFAX_API_PROD = 'https://apiconnect.equifax.com.au/sys2/soap11/score-seeker-v1-0'
 
-const getEquifaxApiURL = () => IS_PROD ? EQUIFAX_API_PROD : EQUIFAX_API_DEV
-const getUsername = () => EQUIFAX_USERNAME
-const getPassword = () => EQUIFAX_PASSWORD
+const getEquifaxConfig = (isProd: boolean) => {
+  return {
+    url: isProd ? EQUIFAX_API_PROD : EQUIFAX_API_DEV,
+    username: isProd ? EQUIFAX_USERNAME_PROD : EQUIFAX_USERNAME_DEV,
+    password: isProd ? EQUIFAX_PASSWORD_PROD : EQUIFAX_PASSWORD_DEV
+  }
+}
+
+type EquifaxConfig = ReturnType<typeof getEquifaxConfig>
 
 const creditScoreInputs = z.object({
   firstName: z.string(),
@@ -36,8 +44,14 @@ export async function POST(request: NextRequest) {
   let score: string|null = null
   let error: string|null = null
 
-
   try {
+    const headersList = headers()
+    const apiKey = headersList.get('x-api-key')
+    if (!apiKey) throw new Error('API key missing')
+    if (apiKey !== process.env.X_API_KEY) throw new Error('Invalid API key')
+
+    const isProd = headersList.get('env') ==' prod'
+
     if (request.method !== 'POST') throw new Error('Invalid method')
     const body = await request.json()
     const parseResult = creditScoreInputs.safeParse(body)
@@ -45,9 +59,11 @@ export async function POST(request: NextRequest) {
       throw new Error('Invalid request body')
     } 
 
+    const equifaxConfig = getEquifaxConfig(isProd)
+
     const equifaxResponse = await xmlPost({
-      url: getEquifaxApiURL(),
-      body: getEquifaxRequestBody(parseResult.data)
+      url: equifaxConfig.url,
+      body: getEquifaxRequestBody(parseResult.data, equifaxConfig)
     })
     if(!equifaxResponse.ok) {
       console.log(equifaxResponse.json())
@@ -63,7 +79,16 @@ export async function POST(request: NextRequest) {
     console.log(e)
   }
 
-  return NextResponse.json({ score, error }, { status });
+  return NextResponse.json(
+    { score, error }, 
+    { 
+      status,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      }
+    })
 }
 
 const xmlPost = async ({ url, body }: { url: string, body: string })  => {
@@ -77,7 +102,10 @@ const xmlPost = async ({ url, body }: { url: string, body: string })  => {
   })
 }
  
-const getEquifaxRequestBody = ({ firstName, lastName, streetNumber, streetName, streetType, suburb, state, postCode, dateOfBirth }: CreditScoreInputs ) => {
+const getEquifaxRequestBody = (
+  { firstName, lastName, streetNumber, streetName, streetType, suburb, state, postCode, dateOfBirth }: CreditScoreInputs,
+  equifaxConfig: EquifaxConfig
+) => {
   const dateOfBirthDateTime = new Date(dateOfBirth.year, dateOfBirth.month, dateOfBirth.day)
   const dateOfBirthDate = dateOfBirthDateTime.toISOString().split('T')[0]
 
@@ -86,8 +114,8 @@ const getEquifaxRequestBody = ({ firstName, lastName, streetNumber, streetName, 
       <soapenv:Header xmlns:wsa="http://www.w3.org/2005/08/addressing">
           <wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
               <wsse:UsernameToken>
-                  <wsse:Username>${getUsername()}</wsse:Username>
-                  <wsse:Password>${getPassword()}</wsse:Password>
+                  <wsse:Username>${equifaxConfig.username}</wsse:Username>
+                  <wsse:Password>${equifaxConfig.password}</wsse:Password>
               </wsse:UsernameToken>
           </wsse:Security>
           <wsa:To>https://vedaxml.corp.dmz/sys2/soap11/score-seeker-v1-0</wsa:To>
---
+++ next.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  i18n: {
+    locales: ['en', 'fr', 'de'],
+    defaultLocale: 'en',
+  },
+ 
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+}
