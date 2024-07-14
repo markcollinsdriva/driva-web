4a8250a80208a85ffa432f1e6f3c7e368d715a58
Mark <mark.collins@driva.com.au>
2024-07-14 18:12:00 +1000

    start

--- app/credit-score/start/page.tsx
+++ app/credit-score/start/page.tsx
@@ -1,7 +1,39 @@
+'use client'
+
+import React, { useState } from 'react'
+import { 
+  VStack, 
+  Box, 
+  FormControl, 
+  FormLabel, 
+  Input,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  FormErrorMessage,
+  Button,
+  Link
+ } from '@chakra-ui/react'
+import { Formik, Field, Form } from 'formik';
+
+import OtpInput from 'react-otp-input';
+
 export default function Page() {
+  const [otp, setOtp] = useState('');
   return (
-    <main>
-      <div>Start</div>
-    </main>
+    <Box>
+      <Box>We've send a code to your phone ending in 9666</Box>
+      <OtpInput
+        value={otp}
+        onChange={setOtp}
+        numInputs={4}
+        renderSeparator={<span>-</span>}
+        renderInput={(props) => <input {...props} />}
+      />
+      <Link>Send again</Link>
+      <Box>Incorrect code. Try again</Box>
+    </Box>
   );
 }
+
